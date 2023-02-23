afff740592e6cca724bfec297e7a0f4238961894
Dmitriy Mikhalev <fakeforall@mail.ru>
2023-02-23 13:22:28 +0500

    Add management command for loading ingredients, move files with data into api app, register all models without customizing.

--- backend/foodgram/api/admin.py
+++ backend/foodgram/api/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import Tag
+from .models import Cart, Favorite, Ingredient, IngredientAmount, Recipe, Tag
 
+admin.site.register(Cart)
+admin.site.register(Favorite)
+admin.site.register(Ingredient)
+admin.site.register(IngredientAmount)
+admin.site.register(Recipe)
 admin.site.register(Tag)
